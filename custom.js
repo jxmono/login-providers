ff398f5c15ec505d13fd3e594aa3379ab8158cdb
Ionică Bizău <bizauionica@yahoo.com>
2014-04-09 10:20:18 +0300

    Comments and syntax

--- custom.js
+++ custom.js
@@ -1,32 +1,47 @@
-
+/**
+ *  Finds a user by providing datasource, userdata and a callback function
+ *
+ */
 function findUser (ds, userData, callback) {
 
     // try to find first this user if he already used this provider
     getByProviderId(ds, userData.id, function(err, user) {
 
+        // handle error
         if (err) { return callback(err); }
 
+        // user found
         if (user) { return callback(null, user); }
 
         // now try to find a user having the sae provider email
         getByProviderEmail(ds, userData.email, function(err, user) {
 
+            // handle error
             if (err) { return callback(err); }
 
+            // user found
             callback(null, user);
         });
     });
 }
 
+/**
+ *  Logs in a user
+ *  If the user is not registered, he will be automatically registered
+ *
+ */
 exports.login = function (link, userData, callback) {
 
+    // get datasource
     M.datasource.resolve(link.params.ds, function(err, ds) {
 
+        // handle error
         if (err) { return callback(err); }
 
         // find the user with matching this provider data
         findUser(ds, userData, function(err, user) {
 
+            // handle error
             if (err) { return callback(err); }
 
             // the user already has an account
@@ -35,8 +50,10 @@ exports.login = function (link, userData, callback) {
                 // this will update only the info for the requested provider
                 update(ds, user, userData, function(err, user) {
 
+                    // handle error
                     if (err) { return callback(err); }
 
+                    // callback
                     callback(null, user._id.toString(), "*", prepareForSession(user));
                 });
 
@@ -46,18 +63,26 @@ exports.login = function (link, userData, callback) {
             // it's a new user that has to be registered
             register(ds, userData, function(err, user) {
 
+                // handle error
                 if (err) { return callback(err); }
 
+                // callback
                 callback(null, user._id.toString(), "*", prepareForSession(user));
             });
         });
     });
 };
 
+/**
+ *  Returns an object containing the session data for the logged in user
+ *
+ */
 function prepareForSession (user) {
 
     // find the current provider
     var providerLogin;
+
+    // search in user logins
     for (var i in user.logins) {
         if (user.logins[i].provider === user.last_login.provider) {
             providerLogin = user.logins[i];
@@ -68,6 +93,7 @@ function prepareForSession (user) {
     // just in case
     providerLogin.auth = providerLogin.auth || {};
 
+    // build session data object
     var sessionData = {
         // the last_login has just been updated to the current provider
         provider: providerLogin.provider,
@@ -80,59 +106,79 @@ function prepareForSession (user) {
     return sessionData;
 }
 
-/*
- * Get user by the provider-specific id
+/**
+ *  Get user by the provider-specific id
+ *
  */
 function getByProviderId (ds, id, callback) {
 
+    // no id
     if (!id) { return callback('Missing user provider id'); }
 
+    // get database
     M.database.open(ds, function(err, db) {
 
+        // handle error
         if (err) { return callback(err); }
 
+        // get collection
         db.collection(ds.collection, function(err, collection) {
 
+            // handle error
             if (err) { return callback(err); }
 
+            // find the provider
             collection.findOne({ "logins.id": id }, callback);
         });
-    }); 
+    });
 }
 
-/*
- * Get user by the provider-specific email
+/**
+ *  Get user by the provider-specific email
+ *
  */
 function getByProviderEmail (ds, email, callback) {
 
+    // no email
     if (!email) { return callback('Missing user provider email'); }
 
+    // get database
     M.database.open(ds, function(err, db) {
 
+        // handle error
         if (err) { return callback(err); }
 
+        // get collection
         db.collection(ds.collection, function(err, collection) {
 
+            // handle error
             if (err) { return callback(err); }
 
+            // find provider by email
             collection.findOne({ "logins.email": email }, callback);
         });
-    }); 
+    });
 }
 
-/*
- * Update user data for a provider
+/**
+ *  Update user data for a provider
+ *
  */
 function update (ds, user, newProviderData, callback) {
 
+    // no data
     if (!user || !newProviderData) { return callback('Invalid user or provider data during login update'); }
 
+    // open database
     M.database.open(ds, function(err, db) {
 
+        // handle error
         if (err) { return callback(err); }
 
+        // open collection
         db.collection(ds.collection, function(err, collection) {
 
+            // handle error
             if (err) { return callback(err); }
 
             // if the user already has this login, update it
@@ -151,26 +197,30 @@ function update (ds, user, newProviderData, callback) {
                 user.logins.push(newProviderData);
             }
 
+            // last login
             var lastLogin = {
-                date: new Date().getTime(),
-                provider: newProviderData.provider
-            };
-
-            var updates = {
-                $set: {
-                    last_login: lastLogin,
-                    logins: user.logins
+                    date: new Date().getTime()
+                  , provider: newProviderData.provider
                 }
-            };
+              , updates = {
+                    $set: {
+                        last_login: lastLogin
+                      , logins: user.logins
+                    }
+                }
+              ;
 
+            // update the user data
             collection.findAndModify({ _id: user._id }, [], updates, { new: true }, function(err, user) {
 
+                // handle error
                 if (err) { return callback(err); }
 
+                // callback user
                 callback(null, user);
             });
         });
-    }); 
+    });
 }
 
 /*
@@ -179,36 +229,44 @@ function update (ds, user, newProviderData, callback) {
  */
 function register (ds, providerData, callback) {
 
+    // missing data
     if (!providerData) { return callback("Missing provider data during registration"); }
 
+    // open database
     M.database.open(ds, function(err, db) {
 
+        // handle error
         if (err) { return callback(err); }
 
+        // open collection
         db.collection(ds.collection, function(err, collection) {
 
+            // handle error
             if (err) { return callback(err); }
 
             // build a new user
             var user = {
-                email: providerData.email,
-                last_login: {
-                    date: new Date().getTime(),
-                    provider: providerData.provider
-                },
-                projects: [],
-                logins: [ providerData ]
+                email: providerData.email
+              , last_login: {
+                    date: new Date().getTime()
+                  , provider: providerData.provider
+                }
+              , projects: []
+              , logins: [providerData]
             };
 
+            // insert the new user
             collection.insert(user, function(err, docs) {
 
+                // handle error
                 if (err) { return callback(err); }
 
+                // no docs
                 if (!docs[0]) { return callback('Registration failed'); }
 
+                // success
                 callback(null, docs[0]);
             });
         });
-    }); 
+    });
 }
-
